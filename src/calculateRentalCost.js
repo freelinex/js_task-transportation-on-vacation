e8e754f703b36613433c434172bebeebfa54e000
Daniil Chursinov <freelinex7@gmail.com>
2025-11-06 20:59:13 +0200

    added a variables, сhanged condition in line 20

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -5,16 +5,20 @@
  */
 function calculateRentalCost(days) {
   // write code here
+  const costPerDay = 40;
+  const discount = 20;
+  const longTermDiscount = 50;
+
   if (days >= 7) {
-    return days * 40 - 50;
+    return days * costPerDay - longTermDiscount;
   }
 
   if (days >= 3) {
-    return days * 40 - 20;
+    return days * costPerDay - discount;
   }
 
-  if (days <= 2) {
-    return days * 40;
+  if (days < 3) {
+    return days * costPerDay;
   }
 }
 
